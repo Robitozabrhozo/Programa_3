0802bc7b4666b9d5938c82503f4dbed8ff01f706
Robitozabrhozo <94878174+Robitozabrhozo@users.noreply.github.com>
2021-11-23 21:33:52 -0600

    INTERFAZ GRAFICA 1

--- Programa_3_Cristhian_UU.py
+++ Programa_3_Cristhian_UU.py
@@ -57,13 +57,53 @@ def jugar():
     
     #se crea la ventana del juego
     juego=tk.Toplevel(ventana)
-    juego.title('2048')
+    juego.title('Sudoku')
     juego.geometry('900x740')
-    juego.config(bg='#ffdb80')
+    juego.config(bg='#b2ff17')
     juego.resizable(False,False)
 
-    
 
+    #se crean los frames a utilizar:
+
+    titulo_frame = tk.Frame(juego,height=128,width=540)
+    titulo_frame.grid(row=0,column=0,padx=1,pady=1)
+    
+    nombre_frame = tk.Frame(juego,bg='#b2ff17',height=128,width=350)
+    nombre_frame.grid(row=0,column=1,padx=1,pady=1)
+
+    partida_frame = tk.Frame(juego,bg="#05ffa3",height=450,width=540)
+    partida_frame.grid(row=1,column=0,padx=1,pady=1)
+
+    numeral_frame = tk.Frame(juego,bg="#ffdb80",height=450,width=350)
+    numeral_frame.grid(row=1,column=1,padx=1,pady=1)
+    #se hace un frame dentro del frame de numeral
+    numeros_frame = tk.Frame(numeral_frame,bg="#0548ff",height=225,width=350)
+    numeros_frame.grid(row=0,column=0,padx=1,pady=1)
+    
+    botones_frame = tk.Frame(numeral_frame,bg="#4c05ff",height=225,width=350)
+    botones_frame.grid(row=1,column=0,padx=1,pady=1)
+    ################################################
+    cronometro_frame = tk.Frame(juego,bg="#c105ff",height=315,width=540)
+    cronometro_frame.grid(row=2,column=0,padx=1,pady=1)
+
+    botones2_frame = tk.Frame(juego,bg="#ff05e6",height=315,width=350)
+    botones2_frame.grid(row=2,column=1,padx=1,pady=1)
+
+    #se crean los botones e interfaz Gráfica
+
+    titulo = tk.Label(titulo_frame,text='Sudoku',font=("Arial Black",54),bg='#b2ff17')
+    titulo.grid(row=0,column=0)
+
+    nombre_label=tk.Label(nombre_frame,text='Nombre del jugador',font=("Arial Black",12),bg='#b2ff17')
+    nombre_label.grid(row=0,column=0)
+    
+    nombre_entry=tk.Entry(nombre_frame,bg='#ffffff',width=40) 
+    nombre_entry.grid(row=1,column=0)
+
+    nombre_boton= tk.Button(nombre_frame,text=' Guardar \n Nombre',
+                              bg='#00d9fa', activebackground = '#3091db',height=2,width=10 )
+    nombre_boton.grid(row=2,column=0,pady=2)
+    
 
 
 
