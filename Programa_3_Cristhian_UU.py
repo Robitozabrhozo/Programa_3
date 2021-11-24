29a3934579b9b0a3959195004fb6c3f9ded10e44
Robitozabrhozo <94878174+Robitozabrhozo@users.noreply.github.com>
2021-11-24 14:50:24 -0600

    Interfaz_grafica(tablero, numeral)

--- Programa_3_Cristhian_UU.py
+++ Programa_3_Cristhian_UU.py
@@ -20,7 +20,7 @@ import pickle
 #Creamos la ventana principal
 ventana=tk.Tk()
 ventana.title('Juego Sudoku')
-ventana.geometry('340x70')
+ventana.geometry('340x330')
 ventana.config(bg='grey')
 ventana.resizable(False,False)
 inicio = tk.Label(ventana,text='JuegoSudoku',
@@ -32,22 +32,31 @@ inicio.grid(row=0,column=0)
 
 
 #barra de menu y sus componentes
-menubar=tk.Menu(ventana)
 
-menubar.add_cascade(label='Jugar',command= lambda:jugar() )
+jugar_boton= tk.Button(ventana,text='Jugar',
+                              bg='#00d9fa', activebackground = '#3091db',height=2,width=10 ,command= lambda:jugar())
+jugar_boton.grid(row=1,column=0,pady=5)
 
-menubar.add_cascade(label='Configurar',command= lambda:configurar())
+configurar_boton= tk.Button(ventana,text='Configurar',
+                              bg='#00d9fa', activebackground = '#3091db',height=2,width=10 ,command= lambda:configurar())
+configurar_boton.grid(row=2,column=0,pady=5)
 
-menubar.add_cascade(label='Ayuda',command= lambda:ayuda() )
-
-menubar.add_cascade(label='Acerca de',command= lambda:showmessage ('Acerca del Programa ',
+ayuda_boton= tk.Button(ventana,text='Ayuda',
+                              bg='#00d9fa', activebackground = '#3091db',height=2,width=10,command= lambda:ayuda() )
+ayuda_boton.grid(row=3,column=0,pady=5)
+    
+Acerca_de_boton= tk.Button(ventana,text='Acerca de',
+                              bg='#00d9fa', activebackground = '#3091db',height=2,width=10 ,command= lambda:showmessage ('Acerca del Programa ',
                                                                  'Creador: Cristhian UU \n'
                                                                    'Nombre del programa: programa_3_Cristhian_Ureña_Ureña'
-                                                                 '\n Version:1.0 \n Fecha de creacion 1/12/2021' ) )
+                                                                 '\n Version:1.0 \n Fecha de creacion 1/12/2021' ))
+Acerca_de_boton.grid(row=4,column=0,pady=5)
 
-menubar.add_cascade(label='Salir',command= lambda: salir() )
+Salir_boton= tk.Button(ventana,text='Salir',
+                              bg='#00d9fa', activebackground = '#3091db',height=2,width=10 ,command= lambda: salir() )
+Salir_boton.grid(row=5,column=0,pady=5)
 
-ventana.configure(menu=menubar)
+
 
 
 
@@ -71,13 +80,13 @@ def jugar():
     nombre_frame = tk.Frame(juego,bg='#b2ff17',height=128,width=350)
     nombre_frame.grid(row=0,column=1,padx=1,pady=1)
 
-    partida_frame = tk.Frame(juego,bg="#05ffa3",height=450,width=540)
+    partida_frame = tk.Frame(juego,bg="#b2ff17",height=450,width=540)
     partida_frame.grid(row=1,column=0,padx=1,pady=1)
 
     numeral_frame = tk.Frame(juego,bg="#ffdb80",height=450,width=350)
     numeral_frame.grid(row=1,column=1,padx=1,pady=1)
     #se hace un frame dentro del frame de numeral
-    numeros_frame = tk.Frame(numeral_frame,bg="#0548ff",height=225,width=350)
+    numeros_frame = tk.Frame(numeral_frame,bg="#b2ff17",height=225,width=350)
     numeros_frame.grid(row=0,column=0,padx=1,pady=1)
     
     botones_frame = tk.Frame(numeral_frame,bg="#4c05ff",height=225,width=350)
@@ -91,9 +100,11 @@ def jugar():
 
     #se crean los botones e interfaz Gráfica
 
+    #TITULO
     titulo = tk.Label(titulo_frame,text='Sudoku',font=("Arial Black",54),bg='#b2ff17')
     titulo.grid(row=0,column=0)
 
+    #NOMBRE
     nombre_label=tk.Label(nombre_frame,text='Nombre del jugador',font=("Arial Black",12),bg='#b2ff17')
     nombre_label.grid(row=0,column=0)
     
@@ -105,7 +116,69 @@ def jugar():
     nombre_boton.grid(row=2,column=0,pady=2)
     
 
+    #CUADRO DE JUEGO
+    matriz=[]
+    
+    for x in range (3):
+        for y in range (3):
+            partida2_frame = tk.Frame(partida_frame,bg="#b2ff17")
+            partida2_frame.grid(row=x,column=y,padx=5,pady=5)
+            for i in range(3):
+                fila = []
+                for j in range(3):
+                    cuadro =  tk.Button(partida2_frame,text='',
+                                      bg='#e4e9f2', activebackground = '#a5a9b0',height=2,width=6,state='disabled' )
+                    cuadro.grid(row=i,column=j,padx=2,pady=2)
+                    fila.append(cuadro)
+                matriz.append(fila)
 
+    #NUMEROS
+    
+
+    boton_1 =  tk.Button(numeros_frame,text='1',
+                              bg='#29f2de', activebackground = '#51db7b',height=1,width=3,font=("Arial Black",14))
+    boton_1.grid(row=0,column=0,padx=3,pady=3)
+
+    boton_2 =  tk.Button(numeros_frame,text='2',
+                              bg='#29f2de', activebackground = '#51db7b',height=1,width=3,font=("Arial Black",14))
+    boton_2.grid(row=0,column=1,padx=3,pady=3)
+
+    boton_3 =  tk.Button(numeros_frame,text='3',
+                              bg='#29f2de', activebackground = '#51db7b',height=1,width=3,font=("Arial Black",14))
+    boton_3.grid(row=0,column=2,padx=3,pady=3)
+    ###########################################
+    boton_4 =  tk.Button(numeros_frame,text='4',
+                              bg='#29f2de', activebackground = '#51db7b',height=1,width=3,font=("Arial Black",14))
+    boton_4.grid(row=1,column=0,padx=3,pady=3)
+
+    boton_5 =  tk.Button(numeros_frame,text='5',
+                              bg='#29f2de', activebackground = '#51db7b',height=1,width=3,font=("Arial Black",14))
+    boton_5.grid(row=1,column=1,padx=3,pady=3)
+
+    boton_6 =  tk.Button(numeros_frame,text='6',
+                              bg='#29f2de', activebackground = '#51db7b',height=1,width=3,font=("Arial Black",14))
+    boton_6.grid(row=1,column=2,padx=3,pady=3)
+    ###########################################
+    boton_7 =  tk.Button(numeros_frame,text='7',
+                              bg='#29f2de', activebackground = '#51db7b',height=1,width=3,font=("Arial Black",14))
+    boton_7.grid(row=2,column=0,padx=3,pady=3)
+
+    boton_8 =  tk.Button(numeros_frame,text='8',
+                              bg='#29f2de', activebackground = '#51db7b',height=1,width=3,font=("Arial Black",14))
+    boton_8.grid(row=2,column=1,padx=3,pady=3)
+
+    boton_9 =  tk.Button(numeros_frame,text='9',
+                              bg='#29f2de', activebackground = '#51db7b',height=1,width=3,font=("Arial Black",14))
+    boton_9.grid(row=2,column=2,padx=3,pady=3)
+    ###########################################
+
+
+
+    #BOTONES DE JUEGO
+    
+    #CRONOMETRO Y TIMER
+    
+    #BOTONES DE GUARDAR Y CARGAR
 
 
 
